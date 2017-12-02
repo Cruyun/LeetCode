6b889adc1fddb586d5458a39a1581591b43b796d
Cruyun <1045671796@qq.com>
2017-12-02 23:07:07 +0800

    035

---
+++ 035_Search_Insert_Position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > target) {
+                if (i == 0) {
+                    index = 0;
+                } else if (nums[i - 1] < target) {
+                    index = i;
+                }
+            } else if (nums[i] == target) {
+                index = i;
+            }
+        }
+        if (nums[nums.size() - 1] < target)
+                index = nums.size();
+        return index;
+    }
+};
