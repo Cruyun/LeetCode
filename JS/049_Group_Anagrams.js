d6eb78669ce794dbfed50fd4762074dd915b8d0f
Cruyun <1045671796@qq.com>
2018-09-25 23:05:53 +0800

    049

---
+++ JS/049_Group_Anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = [];
+    strs.map(str => {
+      const key = str.split('').sort().join('');
+      if (!map[key]) {
+        map[key] = []
+      }
+      map[key].push(str)
+    })
+    return Object.values(map)
+  };
